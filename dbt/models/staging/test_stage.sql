8008bee5d61d0575c01421eb1c625f2b622d8544
César CLAVÉ <cesar.clave@devoteam.com>
2026-04-09 09:08:56 +0000

    delete stage file load

--- dbt/models/staging/test_stage.sql
+++
@@ -1,8 +0,0 @@
-{{ config(materialized='view') }}
-
-SELECT
-    $1::STRING AS col1,
-    $2::STRING AS col2,
-    $3::STRING AS col3
-FROM @RAW_DEV.RAW.MY_TEST_STAGE/data.csv
-(FILE_FORMAT => RAW_DEV.RAW.CSV_FORMAT)
